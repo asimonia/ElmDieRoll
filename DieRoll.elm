0c0eea9b1987af83dbbb973e7f85cfbd21ec8895
Alex Simonian <alex.simonian@gmail.com>
2017-05-17 21:45:24 -0500

    Add main

--- DieRoll.elm
+++ DieRoll.elm
@@ -3,3 +3,12 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Random
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
